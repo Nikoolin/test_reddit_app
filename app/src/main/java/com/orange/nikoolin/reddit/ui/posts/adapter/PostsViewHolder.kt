12d18741395318fe1bc6df4c105ab9f723510eba
nikolaienko_d <d.nikolaienko@tickets.ua>
2020-01-10 21:19:45 +0200

    [chg] small improvements

--- app/src/main/java/com/orange/nikoolin/reddit/ui/posts/adapter/PostsViewHolder.kt
+++ app/src/main/java/com/orange/nikoolin/reddit/ui/posts/adapter/PostsViewHolder.kt
@@ -1,5 +1,6 @@
 package com.orange.nikoolin.reddit.ui.posts.adapter
 
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.text.format.DateUtils
 import android.view.View
@@ -8,9 +9,14 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.orange.nikoolin.reddit.R
 import com.orange.nikoolin.reddit.data.models.Post
 import com.orange.nikoolin.reddit.databinding.PostsListItemBinding
+import java.util.concurrent.TimeUnit
 
 class PostsViewHolder(
         private val binding: PostsListItemBinding
@@ -35,25 +41,30 @@ class PostsViewHolder(
     fun bind(item: Post?) {
         binding.apply {
             post = item
-            postTitle.text = post?.title ?: "unknown"
+            postTitle.text = post?.title ?: ""
             authorName.text = itemView.context.resources.getString(R.string.author_name,
-                    post?.author ?: "unknown")
+                    post?.author ?: "")
             postSubreddit.text = itemView.context.resources.getString(R.string.subreddit,
-                    post?.subreddit ?: "unknown")
+                    post?.subreddit ?: "")
             postScore.text = "${post?.score ?: 0}"
             val time = post?.createdUtc ?: 0
-            createdTime.text = DateUtils.getRelativeTimeSpanString(time * 1000)
+            createdTime.text = DateUtils.getRelativeTimeSpanString(TimeUnit.SECONDS.toMillis(time))
             comments.text = "${post?.numComments ?: 0}"
-            if (post?.thumbnail?.startsWith("http") == true) {
-                thumbnailImageView.visibility = View.VISIBLE
-                glide.load(post?.thumbnail ?: android.R.drawable.stat_notify_error)
-                        .centerCrop()
-                        .placeholder(android.R.drawable.ic_menu_gallery)
-                        .into(thumbnailImageView)
-            } else {
-                thumbnailImageView.visibility = View.GONE
-                glide.clear(thumbnailImageView)
-            }
+            thumbnailImageView.visibility = View.VISIBLE
+            glide.load(post?.thumbnail ?: android.R.drawable.stat_notify_error)
+                    .centerCrop()
+                    .placeholder(android.R.drawable.ic_menu_gallery)
+                    .addListener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            thumbnailImageView.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            return false
+                        }
+                    })
+                    .into(thumbnailImageView)
         }
     }
 }
